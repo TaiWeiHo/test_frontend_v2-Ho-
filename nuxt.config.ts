4c5ec394818826c5129a3a12dfa80971e8b44386
Tobby <tobby2588@gmail.com>
2026-01-08 16:02:31 +0800

    修改nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // nuxt.config.ts
-// import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from 'nuxt/config'
 import { presetUno, presetAttributify, presetIcons } from 'unocss'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
-  // 12. vue-tsc 檢查必須通過
+  // vue-tsc 檢查必須通過
   typescript: {
     strict: true,
     typeCheck: true
@@ -66,13 +66,12 @@ export default defineNuxtConfig({
       alwaysRedirect: true,
       fallbackLocale: 'zh-TW',
     },
-    // 修復 i18n 警告
     bundle: {
       optimizeTranslationDirective: false
     }
   },
 
-  // 將原本 uno.config.ts 的設定移到這裡，解決 Windows 路徑報錯
+  // UnoCSS 設定
   unocss: {
     nuxtLayers: true,
     presets: [
@@ -82,31 +81,14 @@ export default defineNuxtConfig({
     ]
   },
 
-  postcss: {
-    plugins: {
-      'postcss-preset-env': {
-        stage: 1,
-        features: {
-          'nesting-rules': true,
-        },
-      },
-      'postcss-nested': {},
-      'postcss-pxtorem': {
-        rootValue: 16,
-        propList: ['*'],
-        exclude: /node_modules/i,
-      },
-      autoprefixer: {
-        overrideBrowserslist: ['last 2 versions', '> 1%'],
-      },
-    },
-  },
+  // ★★★ 關鍵修改：刪除了 postcss 區塊 ★★★
+  // 因為你有安裝 sass，樣式巢狀 (Nesting) 交給 SCSS 處理即可，
+  // 移除 postcss 可以解決 Vercel 部署時 UnoCSS 掃描報錯的問題。
 
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          // 加入這行來忽略型別報錯
           // @ts-ignore
           api: 'modern-compiler',
         },
